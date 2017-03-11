6f45d4f58cfdf3cdaca266d2b3ee44cb8b46cee2
MartinBG <MartinBG@abv.bg>
2017-03-11 22:59:00 +0200

    Hungry Garfield

---
+++ src/Exams/ProgrammingBasicsExamFebruary21/Ex01HungryGarfield.java
@@ -0,0 +1,12 @@
+package Exams.ProgrammingBasicsExamFebruary21;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class Ex01HungryGarfield {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
