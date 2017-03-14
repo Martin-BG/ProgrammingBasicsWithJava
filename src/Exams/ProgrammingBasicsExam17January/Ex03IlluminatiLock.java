1d06cf4ff03b12774b916a1f91128720b4677561
MartinBG <MartinBG@abv.bg>
2017-03-14 19:37:49 +0200

    Refactoring

--- src/Exams/ProgrammingBasicsExam17January/Ex03IlluminatiLock.java
+++ src/Exams/ProgrammingBasicsExam17January/Ex03IlluminatiLock.java
@@ -10,26 +10,26 @@ public class Ex03IlluminatiLock {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
 
-        String sideDots = repeatStr(".", n);
-        String midDots = "";
-        String centerDots = repeatStr(".", n - 2);
+        String outsideDots = repeatStr(".", n);
+        String sideDots = "";
+        String midSection = "#" + repeatStr(".", n - 2) + "#";
 
-        String lineStartEnd = sideDots + repeatStr("#", n) + sideDots;
+        String lineStartEnd = outsideDots + repeatStr("#", n) + outsideDots;
 
         System.out.println(lineStartEnd);
 
         for (int i = 1; i < n; i++) {
             if (i <= n / 2) {
                 int numSideDots = n - 2 * i;
-                sideDots = repeatStr(".", numSideDots);
-                midDots = repeatStr(".", n - numSideDots - 2);
+                outsideDots = repeatStr(".", numSideDots);
+                sideDots = repeatStr(".", n - numSideDots - 2);
             } else if (i > n / 2 + 1) {
                 int numSideDots = 2 * i - n;
-                sideDots = repeatStr(".", 2 * i - n);
-                midDots = repeatStr(".", n - numSideDots - 2);
+                outsideDots = repeatStr(".", 2 * i - n);
+                sideDots = repeatStr(".", n - numSideDots - 2);
             }
 
-            System.out.println(sideDots + "##" + midDots + "#" + centerDots + "#" + midDots + "##" + sideDots);
+            System.out.println(outsideDots + "##" + sideDots + midSection + sideDots + "##" + outsideDots);
         }
 
         System.out.println(lineStartEnd);
