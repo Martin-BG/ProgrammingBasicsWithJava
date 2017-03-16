8132a19a88b527e98d76e55e8c9f61cd0b48a36b
MartinBG <MartinBG@abv.bg>
2017-03-17 00:33:29 +0200

    Completed RegEx solution for Winning Ticket problem

--- src/Exams/ExamPreparationI/Ex04WinningTicketRegExSolution.java
+++ src/Exams/ExamPreparationI/Ex04WinningTicketRegExSolution.java
@@ -1,91 +1,47 @@
 package Exams.ExamPreparationI;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * https://judge.softuni.bg/Contests/Practice/Index/453#3
- *  ,,,, , , , , , 23wewer@421342344,3423423423 ,,, 234 ,234 , 2344444, ,,,,,,,,,,            ,,    gh
  */
 public class Ex04WinningTicketRegExSolution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tickets = scanner.nextLine().split("[\\s+,+]{1,}");
-/*        System.out.println("-------");
-        int in = 1;*/
+        String[] tickets = scanner.nextLine().split("([\\s*]|[,*]){1,}");
+
+        Pattern r = Pattern.compile("([@]{6,}|[#]{6,}|[$]{6,}|[\\^]{6,})");
+
         for (String ticket : tickets) {
-        //    System.out.println(in++ + " : " + ticket);
-            ticket = ticket.trim();
+            if (ticket.length() == 0) {
+                continue;
+            } else if (ticket.length() != 20) {
+                System.out.println("invalid ticket");
+                continue;
+            }
+
             if (ticket.length() == 20) {
-                char winningChar = '\0';
-                char currentChar = '\0';
-                int longestSequence = 0;
-                int currentSequence = 0;
+                Matcher firstMatch = r.matcher(ticket.substring(0, 10));
+                Matcher secondMatch = r.matcher(ticket.substring(10));
 
-                for (int j = 0; j < 10; j++) {
-                    switch (ticket.charAt(j)) {
-                        case '@':
-                        case '#':
-                        case '$':
-                        case '^':
-                            if (currentChar != ticket.charAt(j)) {
-                                if (currentSequence > longestSequence) {
-                                    longestSequence = currentSequence;
-                                    winningChar = currentChar;
-                                }
-                                currentChar = ticket.charAt(j);
-                                currentSequence = 1;
-                            } else {
-                                currentSequence++;
-                            }
-                            break;
-                        default:
-                            if (currentSequence > longestSequence) {
-                                longestSequence = currentSequence;
-                                winningChar = currentChar;
-                            }
-                            currentSequence = 0;
-                            currentChar = '\0';
-                            break;
+                if (firstMatch.find() && secondMatch.find()
+                        && (firstMatch.group().charAt(0) == secondMatch.group().charAt(0))) {
+
+                    char winChar = firstMatch.group().charAt(0);
+                    int longestSequence = Math.min(firstMatch.group().length(), secondMatch.group().length());
+
+                    if (longestSequence == 10) {
+                        System.out.printf("ticket \"%s\" - %d%c Jackpot!%n", ticket, longestSequence, winChar);
+                    } else {
+                        System.out.printf("ticket \"%s\" - %d%c%n", ticket, longestSequence, winChar);
                     }
-                }
-
-                if (currentSequence > longestSequence) {
-                    longestSequence = currentSequence;
-                    winningChar = currentChar;
-                }
-
-                int secondLongestSequence = 0;
-                currentSequence = 0;
-                if (longestSequence >= 6) {
-                    for (int j = 10; j < 20; j++) {
-                        if (ticket.charAt(j) == winningChar) {
-                            currentSequence++;
-                        } else {
-                            if (currentSequence > secondLongestSequence) {
-                                secondLongestSequence = currentSequence;
-                            }
-                            currentSequence = 0;
-                        }
-                    }
-                }
-
-                if (currentSequence > secondLongestSequence) {
-                    secondLongestSequence = currentSequence;
-                }
-
-                longestSequence = Math.min(longestSequence, secondLongestSequence);
-
-                if (longestSequence == 10) {
-                    System.out.printf("ticket \"%s\" - %d%c Jackpot!%n", ticket, longestSequence, winningChar);
-                } else if (longestSequence >= 6) {
-                    System.out.printf("ticket \"%s\" - %d%c%n", ticket, longestSequence, winningChar);
                 } else {
                     System.out.printf("ticket \"%s\" - no match%n", ticket);
                 }
-            } else {
-                System.out.println("invalid ticket");
             }
-        } //System.out.println("-------");
+        }
     }
 }
