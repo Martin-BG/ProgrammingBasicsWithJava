4dbd6b37c31b9747750ba9c8856756739d134e18
MartinBG <MartinBG@abv.bg>
2017-03-21 21:06:39 +0200

    Completed solution for Ex05SoftUniLogo.java

--- src/Exams/ProgrammingBasicsExamMarch18/Ex05SoftUniLogo.java
+++ src/Exams/ProgrammingBasicsExamMarch18/Ex05SoftUniLogo.java
@@ -14,20 +14,31 @@ public class Ex05SoftUniLogo {
         int height = 4 * n - 2;
         int dashes = 1;
         for (int i = 0; i < 2 * n; i++) {
-            int leftSideDotsCount = (width - dashes) / 2;
+            int SideDotsCount = (width - dashes) / 2;
+            String SideDots = repeatStr(".", SideDotsCount);
+            String centerDashes = repeatStr("#", dashes);
+            System.out.println(SideDots + centerDashes + SideDots);
+            dashes += 6;
+        }
+
+        dashes -= 6;
+        for (int i = 0; i < height - 2 * n; i++) {
+            if (i < height - 2 * n - 1) {
+                System.out.print("|");
+            } else {
+                System.out.print("@");
+            }
+            if (i <= n - 2) {
+                dashes -= 6;
+            }
+            int leftSideDotsCount = (width - dashes) / 2 - 1;
             int rightSideDotsCount = (width - dashes) / 2;
+
             String leftSideDots = repeatStr(".", leftSideDotsCount);
             String rightSideDots = repeatStr(".", rightSideDotsCount);
             String centerDashes = repeatStr("#", dashes);
             System.out.println(leftSideDots + centerDashes + rightSideDots);
-            dashes += 6;
         }
-
-
-/*        for (int i = 0; i < height - 2 * n; i++) {
-
-        }*/
-
     }
 
     private static String repeatStr(String strToRepeat, int count) {
